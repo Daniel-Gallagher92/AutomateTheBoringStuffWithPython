35e8e9a535836743e306dd0b3eb9dc9c7eac16eb
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-02-09 09:56:30 -0700

    adds if, else, elif lesson and interactive terminal exercises

---
+++ AutomateTheBoringStuff/if_else_elif.py
@@ -0,0 +1,13 @@
+name = 'Brii'
+if name == 'Daniel':
+  print('Hi there, Daniel')
+elif name == 'Brittani':
+  print('EEE!')
+else:
+  print('Well, it seems I haven\'t met you yet. Hello! What is your name?')
+  print("\n")
+  myName = input()
+print("\n")
+print('Nice to meet you, ' + myName + '!')
+print("\n")
+print('Done')
