afb5b313e70239f92fdf5785c8d066e90a4c2be6
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-02-10 08:32:01 -0700

    Adds continue statement in while loop example

--- AutomateTheBoringStuff/while_loops.py
+++ AutomateTheBoringStuff/while_loops.py
@@ -10,3 +10,11 @@ while name != 'your name':
     name = input()
     print('\n')
 print('Thank You!')
+print('\n')
+
+spam = 0
+while spam < 5:
+    spam = spam + 1
+    if spam == 3:
+        continue
+    print('spam is ' + str(spam))
