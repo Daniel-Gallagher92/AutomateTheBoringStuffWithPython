ed45d2f37b8890e1b200408037dbc126ebc0548b
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-02-10 08:45:24 -0700

    Adds simple while vs for loop example

--- AutomateTheBoringStuff/for_loops.py
+++ AutomateTheBoringStuff/for_loops.py
@@ -1,3 +1,15 @@
 print('My name is')
-for i in range(5):
-  print('Danny Dingbat ' + str(i))
+i = 0
+while i < 5:
+    print('Danny Dingbat ' + str(i))
+    i = i + 1
+# for i in range(5):
+#   print('Danny Dingbat ' + str(i))
+#   print('\n')
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+print('\n')
+
