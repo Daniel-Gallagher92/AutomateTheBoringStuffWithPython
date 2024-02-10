cdfda3590539a3505341c41541536f3dd7ace2d7
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-02-10 08:22:19 -0700

    Adds another while loop example to check name

--- AutomateTheBoringStuff/while_loops.py
+++ AutomateTheBoringStuff/while_loops.py
@@ -2,3 +2,11 @@ spam = 0
 while spam < 5:
     print(' Hiya!')
     spam = spam + 1
+    print('\n')
+
+name = ' '
+while name != 'your name':
+    print('Please enter your name')
+    name = input()
+    print('\n')
+print('Thank You!')
