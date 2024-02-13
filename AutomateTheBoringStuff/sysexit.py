56ae3b579ac86a07a5e93be242e08fd64415fc92
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-02-13 09:42:03 -0700

    Adds notes for importing third party modules using PIP tool

--- AutomateTheBoringStuff/sysexit.py
+++ AutomateTheBoringStuff/sysexit.py
@@ -3,7 +3,11 @@ print('Hey buddy!')
 sys.exit()
 print('L8er')
 
+#sys.exit() will exit your program before executing all of the lines below 
+
 #in this lesson we also imported pyperclip, using the PIP tool in the REPL (python3 is the command used to enter REPL)
 #pyperclip.copy('Hey hey!')
 #pyperclip.paste()
 # returns 'Hey hey!' 
+
+#Reference automatetheboringstuff.com/appendixa for PIP installation instructions
