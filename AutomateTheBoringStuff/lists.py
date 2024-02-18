fab19afbda65fa09169a353fa6b334f100010eb8
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-02-18 07:40:18 -0700

    adds value assignment through index or slice of array

--- AutomateTheBoringStuff/lists.py
+++ AutomateTheBoringStuff/lists.py
@@ -3,5 +3,20 @@
 
 #syntax is 
 spam = ['cat','bat','rat', 'elephant']
-print(spam[1:3])      #Prints ['bat','rat'] but NOT 'elephant' 
+print(spam[1:3])      #Prints ['bat','rat'] but NOT 'elephant' that lives at index positions 3
 
+
+#You can also assign values to index values using singular index position OR the slice function
+
+
+spam = ['10','20','30']
+
+print(spam)
+
+spam[1] = "Mayo"
+
+print(spam)
+
+spam[1:3] = ['CAT','DOG','MONKEY']
+
+print(spam)
