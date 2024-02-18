4b00bc74a367005815ae2d7189d21de4471918ba
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-02-18 07:36:29 -0700

    adds slice function example

---
+++ AutomateTheBoringStuff/lists.py
@@ -0,0 +1,7 @@
+#For the slice function, in python, it works the same as the range function in the way that it will go up to,
+#but NOT include the last item of the slice
+
+#syntax is 
+spam = ['cat','bat','rat', 'elephant']
+print(spam[1:3])      #Prints ['bat','rat'] but NOT 'elephant' 
+
