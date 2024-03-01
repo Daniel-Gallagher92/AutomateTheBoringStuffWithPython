cdaae91b9d7424c817621dc4860146ee525bebba
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-01 08:07:24 -0700

    adds example of append list method

--- AutomateTheBoringStuff/list_methods.py
+++ AutomateTheBoringStuff/list_methods.py
@@ -1,5 +1,15 @@
-spam = ['hey there', 'hello', 'hiya', 'hey!']
+spam = ['hey there', 'hello', 'hiya', 'hey!', 'hey there']
 
 print(spam.index('hello')) #returns 1
 
 print(spam.index('hey!')) #returns 3
+
+print(spam.index('hey there')) #returns the index position of the first time it sees the given value
+
+spam2 = ['cat', 'dog', 'bat']
+
+print(spam2)
+
+spam2.append('moose')
+
+print(spam2)
