1154e46cd2b6688b82612326b21b71ee50592470
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-03 09:09:06 -0700

    adds example of how references work with mutable values

--- AutomateTheBoringStuff/similarities.py
+++ AutomateTheBoringStuff/similarities.py
@@ -3,3 +3,12 @@ name = 'Navi a dog'
 newName = name[0:5] + 'the' + name[6:10]
 
 print(newName)
+
+spam = [0,1,2,3,4,5]
+cheese = spam 
+
+print(spam)
+
+cheese[1] = 'Hello!'
+print(cheese)
+print(spam) 
