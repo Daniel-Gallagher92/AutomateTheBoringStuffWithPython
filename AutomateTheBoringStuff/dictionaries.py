a2ffb01ffbd01e8ca707efba82ce187401062a60
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-04 09:15:10 -0700

    adds key/value pairs example

---
+++ AutomateTheBoringStuff/dictionaries.py
@@ -0,0 +1,6 @@
+# Key/Value pairs example using previous lesson's new line syntax for readability
+myDog = {'size': 'smol',
+          'color': 'black/brown',
+          'disposition': 'snuggly'}
+
+print(myDog['disposition'])
