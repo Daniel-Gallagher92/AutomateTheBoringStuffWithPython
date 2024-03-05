6c9e0129a671ec7a081d91a345b08dd31760ede3
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-05 11:17:14 -0700

    adds comparison operator with lists and dictionaries example

--- AutomateTheBoringStuff/dictionaries.py
+++ AutomateTheBoringStuff/dictionaries.py
@@ -4,3 +4,15 @@ myDog = {'size': 'smol',
           'disposition': 'snuggly'}
 
 print(myDog['disposition'])
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+
+print(a == b) # Evaluates to false because order in lists matters even with identical values 
+
+myOtherDog = { 'color': 'black/brown',
+              'disposition': 'snuggly',
+              'size': 'smol'
+              }
+
+print(myDog == myOtherDog) # Evaluates to true because order does not matter in dictionaries 
