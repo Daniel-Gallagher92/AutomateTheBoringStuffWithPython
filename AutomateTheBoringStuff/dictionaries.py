c51b82a5da1c03087d3296a6279e9e0fbfbaa9ed
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-05 11:37:57 -0700

    adds examples of Dictionary Methods keys(), values() and items()

--- AutomateTheBoringStuff/dictionaries.py
+++ AutomateTheBoringStuff/dictionaries.py
@@ -27,3 +27,28 @@ print('name' in myOtherDog) # Evaluates to False because key does NOT exist in m
 print('size' not in myOtherDog) # Evaluates to False because key does exist in myOtherDog dictionary
 
 print('name' not in myOtherDog) # Evaluates to True because key does NOT exist in myOtherDog dictionary
+
+
+# Dictionary Methods
+
+# Keys method
+
+print(myDog.keys()) # Returns dict_keys(['size', 'color', 'disposition'])
+
+# Values Method
+
+print(myDog.values()) # Returns dict_values(['smol', 'black/brown', 'snuggly'])
+
+# Items Method 
+
+print(myDog.items()) # Returns dict_items([('size', 'smol'), ('color', 'black/brown'), ('disposition', 'snuggly')])
+
+# If you want to return an ACTUAL list, you must pass to the list function 
+
+print(list(myDog.items())) #Returns [('size', 'smol'), ('color', 'black/brown'), ('disposition', 'snuggly')]
+
+print(list(myDog.values())) #Returns ['smol', 'black/brown', 'snuggly']
+
+print(list(myDog.keys())) #Returns ['size', 'color', 'disposition']
+
+#[('size', 'smol'), ('color', 'black/brown'), ('disposition', 'snuggly')] are known as two item tuples
