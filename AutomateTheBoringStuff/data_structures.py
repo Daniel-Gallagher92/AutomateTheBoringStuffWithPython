ebfc2a96ae286d9f3063a6113450d43a52a0f66b
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-06 09:08:40 -0700

    adds the tic tac toe board

--- AutomateTheBoringStuff/data_structures.py
+++ AutomateTheBoringStuff/data_structures.py
@@ -7,3 +7,9 @@ allDogs.append({ 'name': 'Ozzy' , 'age': 14 , 'color': 'grey'})
 allDogs.append({ 'name': 'Navi', 'age': 3 , 'color': 'black/brown'})
 
 print(allDogs)
+
+
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
