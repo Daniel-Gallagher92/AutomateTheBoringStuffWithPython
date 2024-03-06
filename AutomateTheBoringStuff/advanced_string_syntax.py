aeb6bc27ae60ed46c3944235eeade13311934778
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-06 09:59:37 -0700

    adds raw string example

---
+++ AutomateTheBoringStuff/advanced_string_syntax.py
@@ -0,0 +1,14 @@
+# Different use cases of escape character 
+
+print('That is Brittani\'s dog, lil Navi!')
+print()
+print('Hey there!\nHow are you?\nI\'m fine' ) # prints strings in a block rather than one line
+print()
+
+#Raw strings begin with lower case r immediately r''
+
+# as a Raw String, Python interprets the backslash as a literal character, meant to be a part of the string
+
+print(r'That is Brittani\'s dog.') # returns That is Brittani\'s dog. 
+print()
+
