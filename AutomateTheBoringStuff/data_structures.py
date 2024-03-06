044f5f47aabd0fcb354c27de71280435210447af
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-06 09:01:16 -0700

    adds simple example of data structure and using append method

---
+++ AutomateTheBoringStuff/data_structures.py
@@ -0,0 +1,9 @@
+myDog = { 'name': 'Navi', 'age': 3 , 'color': 'black/brown'}
+
+allDogs = []
+
+allDogs.append({ 'name': 'Spud', 'age': 3 , 'color': 'brown'})
+allDogs.append({ 'name': 'Ozzy' , 'age': 14 , 'color': 'grey'})
+allDogs.append({ 'name': 'Navi', 'age': 3 , 'color': 'black/brown'})
+
+print(allDogs)
