52ca4c0ffbdef81ce1b4f0accc8b55ebdb92391f
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-08 07:14:49 -0700

    adds upper() example

---
+++ AutomateTheBoringStuff/string_methods.py
@@ -0,0 +1,11 @@
+spam = 'hello world!'
+
+print(spam.upper()) # Returns HELLO WORLD! 
+
+print(spam) # Still prints original string 'hello world!' because strings are immutable and not able to be modified in place
+
+# If you wanted to modify the value permanently you'd have to do something like: 
+
+spam = spam.upper()
+
+print(spam) # Returns HELLO WORLD! and spam now ONLY contains the uppercase string value
