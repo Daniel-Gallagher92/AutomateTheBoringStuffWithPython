f23ed50a7f8c5ea077d1bf64a1289700f64b78b4
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-15 09:03:12 -0600

    adds example of simple shell script

--- AutomateTheBoringStuff/python_scripts.py
+++ AutomateTheBoringStuff/python_scripts.py
@@ -1,3 +1,9 @@
 #! /usr/bin/env python3
 
+import time
+
+print('Executing script...')
+time.sleep(5)  # Sleep for 5 seconds
+print('Hello world!!!')
+
 print('Hello world!!!')
---
+++ AutomateTheBoringStuff/python_scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Print "hello world"
+echo "hello world"
+
+# Pause for 5 seconds
+sleep 5
