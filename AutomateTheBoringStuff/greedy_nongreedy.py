c09d4b0f38655f8de756f1d15e7f98b464d47e57
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-25 08:12:11 -0600

    adds failing example of ()? for finding optional matches

--- AutomateTheBoringStuff/greedy_nongreedy.py
+++ AutomateTheBoringStuff/greedy_nongreedy.py
@@ -6,3 +6,5 @@ print(mo.group()) # returns Batman
 
 mo = batRegex.search('The Adventures of Batwoman')
 print(mo.group()) # returns Batwoman
+
+mo = batRegex.search('The Adventures of Batwowowowowoman') # returns None since there is no match
