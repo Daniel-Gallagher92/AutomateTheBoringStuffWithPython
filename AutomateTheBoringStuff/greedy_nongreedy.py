a2e98607af73663acd79923a6688d5f388cf4e9a
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-25 08:11:10 -0600

    adds example of ()? for finding optional matches

--- AutomateTheBoringStuff/greedy_nongreedy.py
+++ AutomateTheBoringStuff/greedy_nongreedy.py
@@ -3,3 +3,6 @@ import re
 batRegex = re.compile(r'Bat(wo)?man')
 mo = batRegex.search('The Adventures of Batman')
 print(mo.group()) # returns Batman 
+
+mo = batRegex.search('The Adventures of Batwoman')
+print(mo.group()) # returns Batwoman
