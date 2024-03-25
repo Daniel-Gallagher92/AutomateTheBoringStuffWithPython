a4111a6f2ff7679a69ca7d00360cae7ecb2868db
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-25 08:20:18 -0600

    adds area code example of ()? for finding optional matches

--- AutomateTheBoringStuff/greedy_nongreedy.py
+++ AutomateTheBoringStuff/greedy_nongreedy.py
@@ -8,3 +8,14 @@ mo = batRegex.search('The Adventures of Batwoman')
 print(mo.group()) # returns Batwoman
 
 mo = batRegex.search('The Adventures of Batwowowowowoman') # returns None since there is no match
+
+phoneRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') # compiles the pattern you are looking for 
+mo = phoneRegex.search('My phone number is 415-555-1234. Call me tomorrow') # searches given text
+print(mo.group()) # returns 415-555-1234 
+
+
+# To allow the area code to be optional, use the ()? chars
+
+phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d') # make sure you include the dash from the area code in your optional search, or else Nonetype error occurs
+mo = phoneRegex.search('My phone number is 555-1234. Call me tomorrow')
+print(mo.group()) # returns 555-1234
