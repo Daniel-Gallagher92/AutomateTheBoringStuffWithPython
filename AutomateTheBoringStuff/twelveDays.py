ccd8c09b2065bd5795219fa443b0892fcb92062e
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-26 11:46:04 -0600

    adds twelve days of xmas example to match digits \d, spaces \s and words \w

---
+++ AutomateTheBoringStuff/twelveDays.py
@@ -0,0 +1,22 @@
+import re
+
+lyrics = '''
+12 drummers drummin'
+11 pipers pipin'
+10 lords a leapin'
+9 ladies dancin'
+8 maids milkin'
+7 swans a swimmin'
+6 geese a layin'
+5 golden rings!
+4 calling birds
+3 french hens
+2 turtle doves
+And 1 partridge in a pear tree!
+'''
+
+xmasRegex = re.compile(r'\d+\s\w+') # \d for numeric digits 0-9, \s for space, \w for any letter, numeric digit, or the underscore character. (Think of this as matching “word” characters.)
+
+matchedObject = xmasRegex.findall(lyrics)
+
+print(matchedObject)
