229de31e9c11aa18a572a2a3bd49c4ac4060f406
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-27 09:01:36 -0600

    adds dollar sign example for strings ending with specified re search pattern

--- AutomateTheBoringStuff/carets_and_dollars.py
+++ AutomateTheBoringStuff/carets_and_dollars.py
@@ -1,5 +1,7 @@
 import re 
 
+# caret character example for strings that MUST BEGIN with given pattern
+
 beginsWithHelloRegex = re.compile(r'^Hello')
 
 matchedObject = beginsWithHelloRegex.search('Hello World!')
@@ -7,3 +9,14 @@ matchedObject = beginsWithHelloRegex.search('Hello World!')
 print(matchedObject) # returns <re.Match object; span=(0, 5), match='Hello'>
 
 print(beginsWithHelloRegex.search('He says "Hello"') == None) # returns True since the string does NOT BEGIN with Hello
+
+
+# dollar sign character example for strings that MUST END with given pattern
+
+endsWithWorldRegex = re.compile(r'world!$')
+
+matchedObject = endsWithWorldRegex.search('Hello world!')
+
+print(matchedObject) # returns <re.Match object; span=(6, 12), match='world!'> 
+
+print(endsWithWorldRegex.search('Hello worldasdasdasdasd!') == None)  # returns True since the string does NOT END with world
