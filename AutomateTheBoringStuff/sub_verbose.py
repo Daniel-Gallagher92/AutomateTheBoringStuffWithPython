1dea84644914ef22f954fdf5fa727a6944935ff7
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-29 08:13:58 -0600

    adds sub() example using \w+ for substituting (names) after 'Agent'

--- AutomateTheBoringStuff/sub_verbose.py
+++ AutomateTheBoringStuff/sub_verbose.py
@@ -7,3 +7,9 @@ namesRegex = re.compile(r'Agent \w+')
 mo = namesRegex.findall('Agent Alice gave the secret documents to Agent Bob')
 
 print(mo) # returns ['Agent Alice', 'Agent Bob']
+
+# sub() method use case. First arg is what you will SUB in for other words, Second arg is the string you search
+
+mo = namesRegex.sub('REDACTED', 'Agent Alice gave the secret documents to Agent Bob')
+
+print(mo) # returns REDACTED gave the secret documents to REDACTED
