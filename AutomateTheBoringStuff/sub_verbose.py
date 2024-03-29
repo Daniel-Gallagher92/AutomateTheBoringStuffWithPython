b35495bff1d09acdbcb5e5817c64a4df67bc10fc
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-29 08:35:53 -0600

    adds example of substituting using groups from original text passing a RAW STRING  with \1

--- AutomateTheBoringStuff/sub_verbose.py
+++ AutomateTheBoringStuff/sub_verbose.py
@@ -13,3 +13,15 @@ print(mo) # returns ['Agent Alice', 'Agent Bob']
 mo = namesRegex.sub('REDACTED', 'Agent Alice gave the secret documents to Agent Bob')
 
 print(mo) # returns REDACTED gave the secret documents to REDACTED
+
+# substituting using groups from original text with \1
+
+namesRegex = re.compile(r'Agent (\w)\w*')
+
+mo = namesRegex.sub(r'Agent \1******', 'Agent Alice gave the secret documents to Agent Bob')
+
+# note that the above line passes a raw string. By adding the r prefix to the replacement pattern string,
+# you ensure that backslashes are treated as literal characters, allowing \1 to be interpreted correctly.
+
+
+print(mo) # returns Agent A****** gave the secret documents to Agent B******
