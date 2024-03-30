e998bb972edbb84c694c2aaf4d5fc8d40def235d
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-30 15:49:50 -0600

    shows example of using os module's sep and getcwd() function

--- AutomateTheBoringStuff/file_path.py
+++ AutomateTheBoringStuff/file_path.py
@@ -3,3 +3,7 @@ import os
 path = os.path.join('folder_1', 'folder_2', 'folder_3', 'file.png')
 
 print(path) # returns folder_1/folder_2/folder_3/file.png with appropriate slashes for mac os
+
+print(os.sep) # stores the separator your os will use
+
+print(os.getcwd()) # returns /Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff
