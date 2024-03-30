109fd153a1fa7c0e136f8f0eedd87edd12cf963c
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-30 07:54:26 -0600

    adds project outline notes and imports modules

---
+++ AutomateTheBoringStuff/phone_and_email_extractor.py
@@ -0,0 +1,14 @@
+#!/bin/zsh python3 
+
+import pyperclip, re
+
+# todo: create regex for phone numbers 
+
+# todo: create regex for email addresses 
+
+# todo: get the text off of the clipboard
+
+# todo: extract email and phone num's
+
+# todo: copy extracted phone nums and emails to clipboard 
+
