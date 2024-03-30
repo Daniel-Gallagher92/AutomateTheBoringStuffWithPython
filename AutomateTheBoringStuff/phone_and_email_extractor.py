15611ba120824e8208eb632f545ae8fbe96ce226
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-03-30 08:05:39 -0600

    adds verbose mode phone num regex with comments

--- AutomateTheBoringStuff/phone_and_email_extractor.py
+++ AutomateTheBoringStuff/phone_and_email_extractor.py
@@ -2,7 +2,19 @@
 
 import pyperclip, re
 
-# todo: create regex for phone numbers 
+
+re.compile(r''' 
+# 415-555-1234, 515,1234, (415) 555-1234, 555-0000 ext 12345,  555-0000 ext. 12345, ext. 12345, x12345
+
+((\d\d\d)|(\(\d\d\d\)))? # area code (optional)
+(\s|-)              # seperator
+\d\d\d              # first three digits
+-                   # seperator 
+\d\d\d\d            # last four digits 
+(((ext(\.)?\s)|x)   # extension word-part (optional)
+  (\d{2,5}))?       # extension number part (optional)
+''', re.VERBOSE)
+
 
 # todo: create regex for email addresses 
 
