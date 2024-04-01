7e03539a1ee40c7aa5b1f7fe43f6a584f9621227
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-01 08:20:24 -0600

    adds example of os.path.abspath('') usage

--- AutomateTheBoringStuff/file_path.py
+++ AutomateTheBoringStuff/file_path.py
@@ -15,3 +15,6 @@ print(os.getcwd()) # returns /Users
 print(os.chdir('/Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff'))
 
 print(os.getcwd()) # returns /Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff
+
+print(os.path.abspath('file_path.py')) # returns absolute path of relative file passed in.
+# /Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff/file_path.py
