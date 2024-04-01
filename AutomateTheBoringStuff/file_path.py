0961b4c3254df367726b9f581c5d3b3205be932f
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-01 08:18:15 -0600

    shows example of using os module's chdir('/file/path') function

--- AutomateTheBoringStuff/file_path.py
+++ AutomateTheBoringStuff/file_path.py
@@ -7,3 +7,11 @@ print(path) # returns folder_1/folder_2/folder_3/file.png with appropriate slash
 print(os.sep) # stores the separator your os will use
 
 print(os.getcwd()) # returns /Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff
+
+print(os.chdir('/Users')) #chdir returns NONE although it does change the directory behind the scenes 
+
+print(os.getcwd()) # returns /Users
+
+print(os.chdir('/Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff'))
+
+print(os.getcwd()) # returns /Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff
