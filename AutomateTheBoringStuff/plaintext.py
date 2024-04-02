19277b09c427268d1dc2d47af8c138a7068d3edc
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-02 09:47:34 -0600

    adds a couple of ways to open and read plaintext files

---
+++ AutomateTheBoringStuff/plaintext.py
@@ -0,0 +1,10 @@
+helloFile = open('/Users/danielgallagher/hello.txt')
+
+print(helloFile.read())
+
+helloFile.close()
+
+# with open('/Users/danielgallagher/hello.txt', 'r') as file:
+#     content = file.read()
+#     print(content)
+
