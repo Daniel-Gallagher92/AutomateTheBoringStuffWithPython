586ae38d0325b2cb94ce7c67c12f5f1840e9b23d
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-02 09:56:49 -0600

    adds os module example for finding abspath and using readlines() function

--- AutomateTheBoringStuff/plaintext.py
+++ AutomateTheBoringStuff/plaintext.py
@@ -8,3 +8,28 @@ helloFile.close()
 #     content = file.read()
 #     print(content)
 
+import os
+
+
+# making notes for os module since course changed to pathlib module
+
+# Get the absolute path of the user's home directory
+home_directory = os.path.expanduser("~")
+
+# Concatenate the home directory path with the filename
+file_path = os.path.join(home_directory, "sonnet29.txt")
+
+# Open the file
+sonnetFile = open(file_path)
+
+# Read the lines of the file
+lines = sonnetFile.readlines()
+
+# Close the file (recommended)
+sonnetFile.close()
+
+# Print the lines
+print(lines)
+
+# by using the readlines method, returned, printed in the terminal is the following...
+# ["When, in disgrace with fortune and men's eyes,\n", '\n', 'I all alone beweep my outcast state,\n', '\n', 'And trouble deaf heaven with my bootless cries,\n', '\n', 'And look upon myself and curse my fate,\n']
