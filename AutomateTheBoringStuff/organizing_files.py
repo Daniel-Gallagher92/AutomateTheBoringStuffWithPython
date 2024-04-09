9150d8365cabaf271f5a24498bec60f595e70c00
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-09 10:07:25 -0600

    adds example of shutil.copy() function

--- AutomateTheBoringStuff/organizing_files.py
+++ AutomateTheBoringStuff/organizing_files.py
@@ -1,1 +1,3 @@
 import shutil
+
+shutil.copy('/Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff/organizing_files.py', '/Users/danielgallagher/python/delicious')
