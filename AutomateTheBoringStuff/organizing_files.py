e1330a9941a130ceab38df22c72fca66217bd61d
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-09 10:13:51 -0600

    adds example of renaming file using shutil.copy() function

--- AutomateTheBoringStuff/organizing_files.py
+++ AutomateTheBoringStuff/organizing_files.py
@@ -1,3 +1,7 @@
 import shutil
 
 shutil.copy('/Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff/organizing_files.py', '/Users/danielgallagher/python/delicious')
+
+# below is an example of renaming the file being copied by adding new filename to end of destination.
+
+shutil.copy('/Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff/organizing_files.py', '/Users/danielgallagher/python/delicious/spamalot.txt')
