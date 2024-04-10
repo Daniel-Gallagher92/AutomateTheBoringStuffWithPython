c6746707ab9b1c2ef29342156e2d297d841b59d2
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-10 07:37:57 -0600

    permanently deleting FOLDER AND ALL CONTENTS with shutil.rmtree() example

--- AutomateTheBoringStuff/deleting_files.py
+++ AutomateTheBoringStuff/deleting_files.py
@@ -11,3 +11,9 @@ os.unlink('/Users/danielgallagher/python/delicious_backup/spamalot.txt')
 # permanently delete empty folder, WILL NOT WORK IF ANY FILES OR FOLDERS ARE IN FOLDER TO BE DELETED 
 
 os.rmdir('/Users/danielgallagher/python/delicious/walnut/waffles')
+
+# permanently delete folder and ALL OF ITS CONTENTS :::DANGER!!::: 
+
+import shutil
+
+shutil.rmtree('/Users/danielgallagher/python/delicious_backup')
