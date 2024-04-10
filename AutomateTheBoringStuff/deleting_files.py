50c08f3384bedc374bfca55c7494ef226705e018
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-10 07:43:45 -0600

    dry run with print function instead of actually deleting

--- AutomateTheBoringStuff/deleting_files.py
+++ AutomateTheBoringStuff/deleting_files.py
@@ -1,19 +1,34 @@
 import os
 
-# permanently deleting single file
+# # permanently deleting single file
 
-# you can pass relative path if you know you're in that directory, otherwise, specify absolute path
+# # you can pass relative path if you know you're in that directory, otherwise, specify absolute path
 
-print(os.getcwd())
+# print(os.getcwd())
 
-os.unlink('/Users/danielgallagher/python/delicious_backup/spamalot.txt')
+# os.unlink('/Users/danielgallagher/python/delicious_backup/spamalot.txt')
 
-# permanently delete empty folder, WILL NOT WORK IF ANY FILES OR FOLDERS ARE IN FOLDER TO BE DELETED 
+# # permanently delete empty folder, WILL NOT WORK IF ANY FILES OR FOLDERS ARE IN FOLDER TO BE DELETED 
 
-os.rmdir('/Users/danielgallagher/python/delicious/walnut/waffles')
+# os.rmdir('/Users/danielgallagher/python/delicious/walnut/waffles')
 
-# permanently delete folder and ALL OF ITS CONTENTS :::DANGER!!::: 
+# # permanently delete folder and ALL OF ITS CONTENTS :::DANGER!!::: 
 
-import shutil
+# import shutil
 
-shutil.rmtree('/Users/danielgallagher/python/delicious_backup')
+# shutil.rmtree('/Users/danielgallagher/python/delicious_backup')
+
+
+
+# RECOMMENDED: DRY RUN
+
+os.chdir('/Users/danielgallagher/python/delicious')
+
+for filename in os.listdir():
+  if filename.endswith('.txt'):
+    #os.unlink(fileName)
+    print(filename) # returns filler2.txt
+#                             spam.txt
+#                             spamalot.txt
+#                             filler.txt
+#     If all the returned filenames are the ones you intend to delete, uncomment call to os.link(fileName)
