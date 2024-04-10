cb8e9e05af8c9be29b1e5d83f18e5b9b4b902318
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-10 07:36:18 -0600

    permanently deleting EMPTY folder with os.rmdir() example

--- AutomateTheBoringStuff/deleting_files.py
+++ AutomateTheBoringStuff/deleting_files.py
@@ -1,4 +1,13 @@
 import os
 
 # permanently deleting single file
+
+# you can pass relative path if you know you're in that directory, otherwise, specify absolute path
+
+print(os.getcwd())
+
 os.unlink('/Users/danielgallagher/python/delicious_backup/spamalot.txt')
+
+# permanently delete empty folder, WILL NOT WORK IF ANY FILES OR FOLDERS ARE IN FOLDER TO BE DELETED 
+
+os.rmdir('/Users/danielgallagher/python/delicious/walnut/waffles')
