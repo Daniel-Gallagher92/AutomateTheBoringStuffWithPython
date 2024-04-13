5af44a6cc3d461ad2aa4f8768594992c555cfc4b
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-13 08:11:54 -0600

    adds notes and removes commented out code

--- AutomateTheBoringStuff/walking.py
+++ AutomateTheBoringStuff/walking.py
@@ -1,5 +1,6 @@
 import os
 
+
 for folderName, subfolders, filenames in os.walk('/Users/danielgallagher/python/delicious'):
   print('The folder is ' + folderName)
   print('The subfolders in ' + folderName + ' are: ' + str(subfolders))
@@ -9,9 +10,8 @@ for folderName, subfolders, filenames in os.walk('/Users/danielgallagher/python/
 
 # the code below will check if any subfolders are named Eggs, and if so, delete them. 
   #can confirm, this works
-  
+
   for subfolder in subfolders:
     if 'eggs' in subfolder:
       subfolderPath = os.path.join(folderName, subfolder)
       os.rmdir(subfolderPath)
-      # print(subfolderPath)
