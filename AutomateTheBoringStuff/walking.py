2706010ea133acb2727de9ca9d06f4771852270c
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-13 08:07:38 -0600

    delete directory if match name while walking directory tree

--- AutomateTheBoringStuff/walking.py
+++ AutomateTheBoringStuff/walking.py
@@ -5,3 +5,13 @@ for folderName, subfolders, filenames in os.walk('/Users/danielgallagher/python/
   print('The subfolders in ' + folderName + ' are: ' + str(subfolders))
   print('The filenames in ' + folderName + ' are: ' + str(filenames))
   print()
+
+
+# the code below will check if any subfolders are named Eggs, and if so, delete them. 
+  #can confirm, this works
+  
+  for subfolder in subfolders:
+    if 'eggs' in subfolder:
+      subfolderPath = os.path.join(folderName, subfolder)
+      os.rmdir(subfolderPath)
+      # print(subfolderPath)
