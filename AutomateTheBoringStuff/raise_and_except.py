faa267e5985aa683b4aeaf05d05be045fc0dab47
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-14 14:18:32 -0600

    boxPrint program

--- AutomateTheBoringStuff/raise_and_except.py
+++ AutomateTheBoringStuff/raise_and_except.py
@@ -5,3 +5,14 @@ raise Exception('This is the helpful error message')
   #File "/Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff/raise_and_except.py", line 1, in <module>
     #raise Exception('This is the helpful error message')
 #Exception: This is the helpful error message
+
+def boxPrint(symbol, width, height):
+  print(symbol * width)
+
+  for i in range(max(height - 2,0)):
+    print(symbol + (' ' * (width - 2)) + symbol) 
+
+  print(symbol * width)
+
+
+boxPrint('*', 15, 5)
