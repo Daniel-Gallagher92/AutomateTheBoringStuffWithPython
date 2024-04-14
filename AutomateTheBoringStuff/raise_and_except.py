a53f940995987977cf5498e95f8eccc7d4f5579a
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-14 13:56:43 -0600

    raise Exception('error message string') example

---
+++ AutomateTheBoringStuff/raise_and_except.py
@@ -0,0 +1,7 @@
+raise Exception('This is the helpful error message')
+
+# returns in terminal:
+#Traceback (most recent call last):
+  #File "/Users/danielgallagher/python/automate_the_boring_stuff/AutomateTheBoringStuffWithPython/AutomateTheBoringStuff/raise_and_except.py", line 1, in <module>
+    #raise Exception('This is the helpful error message')
+#Exception: This is the helpful error message
