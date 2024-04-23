6f6d2b3e1849f4c76c21e87c8ca5e5cbe866e2a4
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-23 08:59:08 -0600

    adds filename= arg to logging.basicConfig function to store debug statements to a file rather than print to terminal

--- AutomateTheBoringStuff/learn_logging.py
+++ AutomateTheBoringStuff/learn_logging.py
@@ -1,7 +1,8 @@
 import logging
+# below using the filename= arg, we can save our log messages to a .txt file in the cwd and it will not print to terminal
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.disable(logging.CRITICAL) # this line disables log messages that follow. Comment out to see logs again
+logging.basicConfig(filename='myProgramLog.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.CRITICAL) # this line disables log messages that follow. Comment out to see logs again
 
 logging.debug('Start of Program')
 # Example: Buggy Factorial Program
