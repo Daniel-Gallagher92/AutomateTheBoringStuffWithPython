1c371741a3b0e9ca8f0b1e0bb2b24262960fe766
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-23 08:39:57 -0600

    creates buggy program to start

--- AutomateTheBoringStuff/learn_logging.py
+++ AutomateTheBoringStuff/learn_logging.py
@@ -4,3 +4,13 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 
 # Example: Buggy Factorial Program
 
+def factorial(n):
+  total = 1 
+
+  for i in range(n + 1):
+    total *= i
+  return total
+
+print(factorial(5))
+
+# returns 0 .... BROKEN! 
