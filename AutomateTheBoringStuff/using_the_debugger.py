4ceb1dad7ebfb3e448529bfec100fa17b1195ed6
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-24 10:23:55 -0600

    BUG FIX: store variables int(input()) buggy adding program

--- AutomateTheBoringStuff/using_the_debugger.py
+++ AutomateTheBoringStuff/using_the_debugger.py
@@ -1,7 +1,11 @@
 print('Enter the first number to add:')
-first = input()
+first = int(input())
 print('Enter the second number to add:')
-second = input()
+second = int(input())
 print('Enter the third number to add:')
-third = input()
-print('The sum of you numbers is: ' + first + second + third )
+third = int(input())
+print('The sum of you numbers is: ', first + second + third )
+
+# setting up breakpoints (red dots) and using the debugger showed the input was storing our integers as strings
+# and this was returning the 3 numbers string concatenated, not mathematically added 
+# by the debugger exposing the data type, this reminds us to store our input as int(input()) to ensure storage as an integer
