c21fa94897ac2b2fdc25007929bc57d757aeab17
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-30 08:03:41 -0600

    Notes

--- AutomateTheBoringStuff/learn_webBrowser.py
+++ AutomateTheBoringStuff/learn_webBrowser.py
@@ -8,3 +8,8 @@ if len(sys.argv) > 1:
   address = ' '.join(sys.argv[1:])
 else:
   address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place' + address)
+
+# all working code and shell scripts are located in MyPythonScripts folder 
+
