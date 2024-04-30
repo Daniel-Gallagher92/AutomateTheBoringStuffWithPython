4e61c3e37a3268527460c4252db2c9059e594bf4
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2024-04-30 08:13:43 -0600

    Bug fix: adds forward slash  to Maps url

--- AutomateTheBoringStuff/learn_webBrowser.py
+++ AutomateTheBoringStuff/learn_webBrowser.py
@@ -1,15 +1,34 @@
-import webbrowser, sys, pyperclip
+# import webbrowser, sys, pyperclip
+
+# # webbrowser.open('https://automatetheboringstuff.com')
+
+# sys.argv #['mapit.py', '870', 'Valencia', 'St.']
+
+# if len(sys.argv) > 1:
+#   address = ' '.join(sys.argv[1:])
+# else:
+#   address = pyperclip.paste()
+
+# webbrowser.open('https://www.google.com/maps/place' + address)
+
+# # all working code and shell scripts are located in MyPythonScripts folder 
+
+# The following code is the working code
+
+# Not included in the course is urllib.parse module.... I've got it to work without it just by adding a forward slash after 'place' in google maps url
+
+import webbrowser, sys, pyperclip, urllib.parse
 
 # webbrowser.open('https://automatetheboringstuff.com')
 
-sys.argv #['mapit.py', '870', 'Valencia', 'St.']
+# sys.argv #['mapit.py', '870', 'Valencia', 'St.']
 
 if len(sys.argv) > 1:
   address = ' '.join(sys.argv[1:])
 else:
   address = pyperclip.paste()
 
-webbrowser.open('https://www.google.com/maps/place' + address)
 
-# all working code and shell scripts are located in MyPythonScripts folder 
+# encoded_address = urllib.parse.quote(address)
 
+webbrowser.open('https://www.google.com/maps/place/' + address)
